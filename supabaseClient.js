3fdb35c8680be1c3f823472c98a75bf9008ae39e
potashev_ve <potashev_ve@vniiftri.ru>
2026-02-06 13:32:07 +0300

    исправление что бы работало на телефоне

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,4 +3,13 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 export const SUPABASE_URL = "https://qznxqgavwemplturysql.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6bnhxZ2F2d2VtcGx0dXJ5c3FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4Mjk2NzEsImV4cCI6MjA4NTQwNTY3MX0.MZGdVpbIw6vfLYHcCsTelWzTsp2CR2rjeOWbuTRu77Y";
 
-export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY ,{
+  global: {
+    fetch: (url, options = {}) =>
+      fetch(url, {
+        ...options,
+        keepalive: true,          // ⭐ ключ к решению
+        cache: "no-store"         // ⭐ ускоряет первый запрос
+      })
+  }
+});
